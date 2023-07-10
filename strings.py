a541b7c4876851f8bd12ca91cce34d7ae94fccc4
kolaynurdan <kolaynurdan@gmail.com>
2023-07-10 03:07:32 +0300

    added strings

---
+++ strings.py
@@ -0,0 +1,14 @@
+name = 'Nurdan'
+surname = 'Kolay'
+age = 30
+
+greeting= 'My name is '+ name +' '+'surname '+surname+' and\nI am '+str(age)
+print(greeting)
+print(greeting[0])
+print(greeting[3])
+print(len(greeting))
+print(greeting[-1]) #last letter
+print(greeting[3:7]) #specified range
+print(greeting[3:]) #starting within index 3 and going up to
+print(greeting[:15]) #starting within index beginning and go up to index 15
+print(greeting[2:40:2]) #Increase by two starting from 2 and going up to 40
