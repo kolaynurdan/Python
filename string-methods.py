2c3386b275a8b8169f953b3c740ec4859c18ca6b
kolaynurdan <kolaynurdan@gmail.com>
2023-07-12 17:05:54 +0300

    Added string-methods

---
+++ string-methods.py
@@ -0,0 +1,14 @@
+#Upper Methods
+
+message = 'Hello There. I am Nurdan'
+message = message.upper()
+print(message)
+message = message.lower()
+print(message)
+message = message.title()
+print(message)
+message = message.capitalize()
+print(message)
+message = message.strip() 
+print(message)
+
