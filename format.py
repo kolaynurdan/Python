9d44b0fb17a3b87296d7f46d62aeb316c375415b
kolaynurdan <kolaynurdan@gmail.com>
2023-07-12 09:23:32 +0300

    added format

--- format.py
+++ format.py
@@ -1,5 +1,14 @@
 name = 'Nurdan'
 surname = 'Kolay'
+age = 30
 
 print('My name is {} {}'.format(name, surname))
 print('My name is {1} {0}'.format(name, surname))
+print("My name is {s} {n} and I am ".format(n=name, s=surname))
+print("My name is {} {} and I am {}.".format(name, surname, age))
+
+result = 200 / 700
+print('the result is {r:1.3}'.format(r=result)) #3 number signs which number after comma.
+print('the result is {r:1.4}'.format(r=result)) #3 number signs which number after comma.
+
+print(f"My name is {name} {surname} and I am {age}.") #we defined first.
