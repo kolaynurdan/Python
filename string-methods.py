8955ceab166dc7a8a77a9703f5792591117a6fe7
kolaynurdan <kolaynurdan@gmail.com>
2023-07-14 02:36:43 +0300

    string-methods

--- string-methods.py
+++ string-methods.py
@@ -1,14 +1,25 @@
-#Upper Methods
+#String Methods
 
 message = 'Hello There. I am Nurdan'
-message = message.upper()
+message = message.upper() #write uppercase each letter
 print(message)
-message = message.lower()
+message = message.lower() #write lowercase each letter 
 print(message)
-message = message.title()
+message = message.title() #just first letter of words upper
 print(message)
-message = message.capitalize()
+message = message.capitalize() #The first letter of the first word is capitalized.
 print(message)
-message = message.strip() 
+message = message.strip() #removes leading and trailing spaces.
 print(message)
+message = message.split() #writes each word separately.
+print(message)
+print(message[0]) #first word
+#message = message.split('.') #writes to separate with '.'
+print(message)
+
+message2 = 'I worked so hard today.'
+message2 = message2.split() #we want to separate list
+print(message2)
+message2 = '*'.join(message2) #we want to join list 
+print(message2)
 
