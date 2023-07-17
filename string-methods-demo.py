f28d1695c43135df98ac57b83e1ef7aad59c6519
kolaynurdan <kolaynurdan@gmail.com>
2023-07-17 22:32:05 +0300

    add files

--- string-methods-demo.py
+++ string-methods-demo.py
@@ -27,5 +27,32 @@ print(s)
 # 3- 'course' make all characters in the string small.
 
 res = course.lower()
+print(res)
 #upper method
 res = course.upper()
+print(res)
+#title method
+res = course.title()
+print(res)
+#4- 'website' how many a characters are in it?
+
+ress = website.count('a')
+print(ress)
+ress = website.count('www')
+print(ress)
+ress = website.count('www',0,10) #range[0,10]
+print(ress)
+
+#5- 'website' starts with 'www', ends with 'com'
+w = website.startswith('www')
+print(w) #bool type
+w = website.startswith('http')
+print(w)
+w = website.endswith('com')
+print(w)
+
+#6- 'website', '.com' char in it?
+variable2 = website.find('.com')
+print(variable2) #indexnumber
+variable3 = website.find('')
+#7- 
