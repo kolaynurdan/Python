d717ad5f8aff6f814cd8694d537efbc4bdb5b8e9
kolaynurdan <kolaynurdan@gmail.com>
2023-08-29 16:30:14 +0300

    added class

--- oop/class.py
+++ oop/class.py
@@ -1,10 +1,19 @@
 #class
 
 class Person:
-    #attributes 
     pass
-
-
+    #attributes 
+    #constructor
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+        print("init methods are worked.")
     #methods
 
 #object instance
+p1 = Person()
+p2 = Person()
+print(p1)
+print(p2)
+print(type(p1))
+print(type(p2))
