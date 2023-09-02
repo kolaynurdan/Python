3db9ccc0b07b1e75dda3f16ed98c5ab14b95e259
kolaynurdan <kolaynurdan@gmail.com>
2023-09-02 13:32:58 +0300

    added inheritance

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -5,12 +5,33 @@
 
 #Animal => Dog(Animal), Cat(Animal)
 class Person():
-    def __init__(self):
+    def __init__(self,fname,lname):
+        self.firstName = fname
+        self.lastName = lname
         print('Person Created')
 
+    def who_am_i(self):
+        print('I am a person')
+
+    def eat(self):
+        print('i am eating')
 
 class Student(Person):
-    pass
+    def __init__(self,fname,lname):
+        Person.__init__(self,fname,lname)
+        print('Student Created')
+    #overriding
+    def who_am_i(self):
+        print('I am a student')
 
-p1 = Person()
-s1 = Student()
+p1 = Person('Ali','Yilmaz')
+s1 = Student('Nurdan','Kolay')
+
+print(p1.firstName, p1.lastName)
+print(s1.firstName, s1.lastName)
+
+p1.who_am_i()
+s1.who_am_i()
+
+p1.eat()
+s1.eat()
