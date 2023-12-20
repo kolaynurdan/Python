1ee9c4b8a684f702c201dd20405d6381695d9943
kolaynurdan <kolaynurdan@gmail.com>
2023-12-21 01:30:19 +0300

    added handling

--- exceptions/handling.py
+++ exceptions/handling.py
@@ -1,4 +1,22 @@
-<<<<<<< HEAD
-=======
-..
->>>>>>> e2d2275fafff98765ae69e1140e9a419a634e97f
+#error handling 
+
+'''
+try:
+    x = int(input('x: '))
+    y = int(input('y: '))
+    print(x/y)
+except ZeroDivisionError:
+    print('You cant enter 0 for y')
+except ValueError:
+    print('you have to enter digit value for x and y')
+'''
+
+try:
+    x = int(input('x: '))
+    y = int(input('y: '))
+    print(x/y)
+except (ZeroDivisionError,ValueError) as a:
+    print('You enter wrong value!')
+    print(a)
+else:
+    break
